d5b2e9afe43c013213e3f771bd8399682ba76896
abdu1478 <abduseid14789@gmail.com>
2025-07-14 16:22:53 +0300

    Fixed redis issue

--- utils/redisClient.js
+++ utils/redisClient.js
@@ -1,18 +1,24 @@
-const { Redis } = require("ioredis");
+const Redis = require("ioredis");
+require("dotenv").config();
 
-const redis = new Redis(process.env.REDIS_URL)
-  // || "redis://localhost:6379");
+if (!global.redis) {
+  if (!process.env.REDIS_URL) {
+    console.error("REDIS_URL is not defined in the environment variables.");
+    process.exit(1);
+  }
 
-redis.on("error", (err) => {
-  console.error("Redis error:", err);
-});
+  redis = new Redis(process.env.REDIS_URL, {
+    tls: {}, 
+    maxRetriesPerRequest: 3,
+    connectTimeout: 5000,
+  });
 
-redis.on("connect", () => {
-  console.log("🔌 Redis connected");
-});
+  redis.on("connect", () => console.log("🔌 Redis connected"));
+  redis.on("error", (err) => console.error("❌ Redis error:", err));
 
-redis.on("error", (err) => {
-  console.error("Redis error:", err);
-});
+  global.redis = redis; 
+} else {
+  redis = global.redis;
+}
 
 module.exports = redis;
